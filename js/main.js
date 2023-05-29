a7bea2ad8d343644127e765203900b3d86a01468
Quang Hai <haimobiarmy2002@gmail.com>
2023-05-30 00:06:22 +0700

    update ngày 30/5/2023

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// input
+// let inputHeader = document.querySelector('.heading-content-search-input');
+// let body = document.querySelector('body');
+// let overlay = document.querySelector('.overlay');
+
+// inputHeader.addEventListener('click', function (e) {
+//     overlay.style.display = 'block';
+//     inputHeader.focus();
+// })
