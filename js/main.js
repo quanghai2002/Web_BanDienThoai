eca1677d3c6e48d8e797890d8c8d4180db1d8ba3
Quang Hai <haimobiarmy2002@gmail.com>
2023-05-31 23:30:36 +0700

    update ngày 31/5/2023

--- js/main.js
+++ js/main.js
@@ -5,3 +5,27 @@ import slider from './slider.js';
 // inpu inputSearch
 inputSearch();
 slider();
+
+
+let upTop = document.querySelector('.up-top');
+// click up top page
+window.onscroll = (function () {
+    // console.log(window.scrollY);
+    if (window.scrollY > 133) {
+        upTop.classList.add('up-top-active');
+
+        upTop.onclick = (e) => {
+
+            document.documentElement.scrollTop = 0;
+        }
+
+    }
+    else {
+        upTop.classList.remove('up-top-active');
+    }
+});
+
+
+
+
+
